797b2f253b2d4245aac5a1371c5a653d4b7fd665
Israel Adetunji <adisen230@gmail.com>
2020-09-03 21:26:30 +0100

    Quiz app developed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App () {
+import QuestionCard from "./QuestionCard";
+
+import questions from "./questions.json";
+
+function App() {
+  const [questionNum, setQuestionNum] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(
+    questions[questionNum]
+  );
+  const [score, setScore] = useState(0);
+  const [showNext, setShowNext] = useState(false);
+  const [displayResult, setDisplayResult] = useState("");
+
+  const numberofQuestions = questions.length;
+
+  const nextQuestion = () => {
+    let nextquestionNum = questionNum + 1;
+    if (nextquestionNum < numberofQuestions) {
+      setQuestionNum(nextquestionNum);
+      setCurrentQuestion(questions[nextquestionNum]);
+      setShowNext(false);
+      setDisplayResult("");
+    }
+  };
+
+  const updateScore = update => {
+    setShowNext(true);
+    if (update) {
+      setDisplayResult("Correct");
+      let currentScore = score + 1;
+      setScore(currentScore);
+    } else {
+      setDisplayResult("Wrong");
+    }
+  };
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Progress bar */}
+      <div className='progress__bar--outer'>
+        <div
+          style={{
+            width: `${((questionNum + 1) / numberofQuestions) * 100}%`,
+          }}
+          id='progress__bar--inner'
+        ></div>
+      </div>
+
+      <div className='container'>
+        {/* Question Card */}
+        <QuestionCard
+          currentQuestion={currentQuestion}
+          numberofQuestions={numberofQuestions}
+          questionNum={questionNum}
+          updateScore={updateScore}
+        />
+
+        {/* Next Button */}
+        <div>
+          <h3>{displayResult}</h3>
+          {showNext && <button onClick={nextQuestion}>Next Question</button>}
+        </div>
+
+        {/* Score tracker */}
+        <div>
+          <div>
+            <p>
+              <span>{`Score: ${(score / numberofQuestions) * 100}%`}</span>
+              <span style={{ float: "right" }}>{`Max Score: ${
+                (score / numberofQuestions) * 100 +
+                ((numberofQuestions - questionNum) / numberofQuestions) * 100
+              }%`}</span>
+            </p>
+
+            <p style={{ float: "right" }}></p>
+          </div>
+
+          <div className='progress__bar--outer'>
+            <div
+              style={{
+                width: `${(score / numberofQuestions) * 100}%`,
+                background: "#000",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                zIndex: 90,
+              }}
+              id='progress__bar--inner'
+            ></div>
+            <div
+              style={{
+                width: `${(questionNum / numberofQuestions) * 100}%`,
+                background: "#a9aaa9",
+                position: "absolute",
+                top: 0,
+                left: 0,
+              }}
+              id='progress__bar--inner'
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/QuestionCard.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+function QuestionCard({
+  currentQuestion,
+  numberofQuestions,
+  questionNum,
+  updateScore,
+}) {
+  console.log(currentQuestion);
+
+  const shuffle = array => {
+    return array.sort(() => Math.random() - 0.5);
+  };
+
+  const answers = [
+    ...currentQuestion.incorrect_answers,
+    currentQuestion.correct_answer,
+  ];
+
+  const checkAnswer = e => {
+    let decodedAnswer = decodeURIComponent(currentQuestion.correct_answer);
+    if (e.target.value === decodedAnswer) {
+      updateScore(true);
+    } else {
+      updateScore(false);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>{`Question ${questionNum + 1} of ${numberofQuestions}`}</h1>
+        <p>{decodeURIComponent(currentQuestion.category)}</p>
+        {currentQuestion.difficulty === "easy" ? (
+          <p>
+            <i class='fas fa-star'></i>
+          </p>
+        ) : currentQuestion.difficulty === "medium" ? (
+          <p>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+          </p>
+        ) : (
+          <p>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+          </p>
+        )}
+      </div>
+
+      <div>
+        <h2>{decodeURIComponent(currentQuestion.question)}</h2>
+        <div id='answers__container'>
+          {shuffle(answers).map(answer => {
+            return (
+              <button
+                key={answer}
+                value={decodeURIComponent(answer)}
+                onClick={checkAnswer}
+                className='answerButton'
+              >
+                {decodeURIComponent(answer)}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default QuestionCard;
